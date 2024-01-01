a45435d1360d693a3aa0ff02aa50ec34053403ed
Westerbly Snaydley <westerbly@gmail.com>
2024-01-01 05:28:44 -0300

    tasks

---
+++ dodo.py
@@ -0,0 +1,44 @@
+import platform
+import os
+from doit.tools import LongRunning
+
+# DOIT_CONFIG = {'default_tasks': ['build_demos']}
+FILTER_ANSI = os.getenv("DOIT_FILTER_ANSI")
+
+def cmd(cmd):
+    if platform.system() == "Linux" and FILTER_ANSI:
+        return f"bash -c \"{cmd} | ansi2txt\" || {cmd}"
+    else:
+        return f"bash -c \"{cmd}\""
+
+def task_eo():
+    return {
+        'actions': [cmd("wget -O eo https://hg.sr.ht/~duangle/majoreo/raw/eo?rev=tip"), cmd("chmod +x eo")],
+        'targets': ["eo"],
+        'uptodate': [True]
+    }
+
+bootstrap = ".eo/installed/bootstrap"
+def task_bootstrap():
+    return {
+        'verbosity': 2,
+        'actions': [cmd("./eo init; true"), cmd("./eo install -y bootstrap")],
+        'file_dep': ["eo"],
+        'targets': [bootstrap]
+    }
+
+def task_force_bootstrap():
+    return {
+        'verbosity': 2,
+        'actions': [cmd("rm -rf .eo lib include"), cmd("./eo init"), cmd("./eo update"), cmd("./eo install -y bootstrap")],
+        'uptodate': [False],
+        'file_dep': ["eo"],
+    }
+
+def task_boot():
+    return {
+        'verbosity': 2,
+        'actions': [LongRunning(f"scopes -e -m .src.main")],
+        'file_dep': [bootstrap],
+        'uptodate': [False]
+    }
--- src/main.sc
+++ src/main.sc
@@ -1,4 +1,10 @@
+using import radl.strfmt radl.version-string
+
+PECO-VERSION := (git-version)
+run-stage;
+
 fn main (argc argv)
+    print f"peco ${PECO-VERSION}"
     0
 
 main 0 0
